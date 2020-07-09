30566551d77c70cb19b8d25c307c87ae68f693a3
Dani Bassett <dani@danithetallgirl.com>
2020-07-09 04:59:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ require 'pry'
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
 
 def using_include(array, element)
-  array.include?("wow", "tardis")
+  array.include?("tardis")
 end
 
 def using_sort(array)
